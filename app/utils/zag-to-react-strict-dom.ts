0c8d1414baee0fba7e54fd6aa2276b5566955f60
malcodeman <26983729+malcodeman@users.noreply.github.com>
2026-04-06 15:12:27 +0200

    feat: filterout autoCorrect prop

--- app/utils/zag-to-react-strict-dom.ts
+++ app/utils/zag-to-react-strict-dom.ts
@@ -1,3 +1,8 @@
-export function zagToReactStrictDom(props: any) {
-  return props;
-}
+// Props that zag-js emits but React Strict DOM does not support
+const INVALID_RSD_PROPS = new Set(["autoCorrect"]);
+
+const isValidRsdProp = ([key]: [string, unknown]) =>
+  !INVALID_RSD_PROPS.has(key);
+
+export const zagToReactStrictDom = (props: Record<string, any>) =>
+  Object.fromEntries(Object.entries(props).filter(isValidRsdProp));
